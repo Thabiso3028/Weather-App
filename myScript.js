a0533c77abb4b0ccdd69cec91172313b8e17f71b
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2022-05-14 00:07:25 +0200

    Key

--- myScript.js
+++ myScript.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "ab7e42b1446f28cf4a2348082c197d37",
+    apiKey: "",
     fetchWeather: function(){
         fetch("https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=ab7e42b1446f28cf4a2348082c197d37"
         ).then((response)=>response.json()).then((data)=>console.log(data));
