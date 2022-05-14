c933ff972d050136a1dcde3844e48cb688e04ff2
Thabiso3028 <Tkubheka.tk1@gmail.com>
2022-05-14 10:01:12 +0200

    API key

--- myScript.js
+++ myScript.js
@@ -1,7 +1,16 @@
+
 let weather = {
     apiKey: "ab7e42b1446f28cf4a2348082c197d37",
-    fetchWeather: function(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=ab7e42b1446f28cf4a2348082c197d37"
-        ).then((response)=>response.json()).then((data)=>console.log(data));
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city + "&unit=metric&appid=" + this.apiKey
+        ).then((response)=>response.json()).then((data)=>this.displayWeather(data));
     },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    }
 };
+ 
